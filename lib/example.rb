946040f34b12fd60e10294a260880c947f4da001
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-06 00:31:22 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,15 +1,6 @@
 # Use this space to code along with the readme
 def greeting
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+puts "Hello World!"
+
 end
-greeting(10)
+greeting
