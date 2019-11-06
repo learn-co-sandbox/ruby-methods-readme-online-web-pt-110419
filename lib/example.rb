b375f6e3b97a3507b7bd906009de90b80433a669
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-06 00:34:12 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -11,4 +11,15 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-#
+# ... The rest of the program
+
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
